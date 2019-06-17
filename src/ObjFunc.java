b447e5faf297c2b2d302f7b27b0eb8079f2cd78a
YoshThePinetree <changebyone8901@gmail.com>
2019-06-17 13:13:25 +0900

    Finished until random particle generation by MT method

--- src/ObjFunc.java
+++ src/ObjFunc.java
@@ -7,6 +7,20 @@ public class ObjFunc {
 		
 		switch(a) {		
 		case 1:		//Rosenbrock function constrained with a cubic and a line
+			// round in the ranges
+			if(x<-1.5) {
+				x = -1.5;
+			}
+			if(x>1.5) {
+				x = 1.5;
+			}
+			if(y<-0.5) {
+				y = -0.5;
+			}
+			if(y>2.5) {
+				y = 2.5;
+			}
+			
 			st1 = Math.pow((x-1),3) - y + 1;
 			st2 = x + y - 2;
 		
@@ -18,6 +32,20 @@ public class ObjFunc {
 			break;
 			
 		case 2:		// Rosenbrock function constrained to a disk
+			// round in the ranges
+			if(x<-1.5) {
+				x = -1.5;
+			}
+			if(x>1.5) {
+				x = 1.5;
+			}
+			if(y<-1.5) {
+				y = -0.5;
+			}
+			if(y>1.5) {
+				y = 1.5;
+			}
+			
 			st1 = x*x + y*y;
 		
 			if(st1<=2) {
@@ -28,6 +56,19 @@ public class ObjFunc {
 			break;
 					
 		case 3:		// Mishra's Bird function - constrained
+			if(x<-10) {
+				x = -10;
+			}
+			if(x>0) {
+				x = 0;
+			}
+			if(y<-6.5) {
+				y = -6.5;
+			}
+			if(y>0) {
+				y = 0;
+			}
+			
 			st1 = Math.pow(x+5,2) + Math.pow(y+5,2);
 			
 			if(st1<25) {
@@ -38,6 +79,14 @@ public class ObjFunc {
 			break;
 			
 		case 4:		// Simionescu function
+			// round in the ranges
+			if(x<-1.25) {
+				x = -1.25;
+			}
+			if(y>1.25) {
+				y = 1.25;
+			}
+			
 			double rt=1, rs=0.2;
 			int n=8;
 			double u=Math.pow( rt + rs*Math.cos(n*Math.atan(x/y)), 2);
@@ -51,6 +100,20 @@ public class ObjFunc {
 			break;
 			
 		case 5:		// 2-D Rastrigin function
+			// round in the ranges
+			if(x<-5.12) {
+				x = -5.12;
+			}
+			if(x>5.12) {
+				x = 5.12;
+			}
+			if(y<-5.12) {
+				y = -5.12;
+			}
+			if(y>5.12) {
+				y = 5.12;
+			}
+			
 			int A=10;
 			f = A*2+ + (x*x - A*Math.cos(2*Math.PI*x)) + (y*y - A*Math.cos(2*Math.PI*y));
 		}
@@ -80,4 +143,54 @@ public class ObjFunc {
 			break;
 		}
 	}
+	
+	// Method to give variable limitation
+	public static double[] GetLimit(int x){
+		double xll=0, xul=0, yll=0, yul=0;
+		
+		switch(x) {
+		case 1:
+			xll = -1.5;
+			xul = 1.5;
+			yll = -0.5;
+			yul = 2.5;
+
+			break;
+		case 2:
+			xll = -1.5;
+			xul = 1.5;
+			yll = -0.5;
+			yul = 1.5;
+			
+			break;
+		case 3:
+			xll = -10;
+			xul = 0;
+			yll = -6.5;
+			yul = 0;
+
+			break;
+		case 4:
+			xll = -1.25;
+			xul = 1.25;
+			yll = -1.25;
+			yul = 1.25;
+			
+			break;
+		case 5:
+			xll = -5.12;
+			xul = 5.12;
+			yll = -5.12;
+			yul = 5.12;
+			
+			break;
+		default :
+			System.out.println("Error");
+			break;
+		}
+		
+		double[] xylim= {xll,xul,yll,yul};
+		return xylim;
+	}
+	
 }
--- src/PSOmain.java
+++ src/PSOmain.java
@@ -1,19 +1,26 @@
 // Main function
+import java.util.Random;
+
 public class PSOmain {
 	public static void main (String arg[]) {
 		System.out.println("PSO: Particle Swarm Optimization");
 		
-		// Initial configuration
-		int fnum=5;			// the function number to solve
+		///////////////////////////
+		// Initial configuration //
+		///////////////////////////
+		
+		int fnum=1;			// the function number to solve
 		int trial=20;		// the number of trials with different random initial
 		int ite=300;		// the number of iterations for a trial
-		int pop=50;			// the number of population
+		int pop=50;			// the number of particles
+		int rseed=1;
 		double w=0.729;		// the PSO inertia coefficient
 		double c1=1.49445;	// the PSO personal coefficient
 		double c2=1.49445;	// the PSO global coefficient
+		double vlim=4.0;	// the particle speed limitation
 		
 		int PG[]= {fnum,trial,ite,pop};
-		double PP[]= {w,c1,c2};
+		double PP[]= {w,c1,c2,vlim};
 		
 		System.out.printf("General Parameters:\n");
 		System.out.printf("trial\titeration\tparticles\n");
@@ -27,6 +34,31 @@ public class PSOmain {
 		
 //		double ans=ObjFunc.EvalFunc(0, 0, fnum);
 //		System.out.printf("%f",ans);
+		
+		///////////////////////////////////////////////
+		// Particle Generation & Initial Evaluation  //
+		///////////////////////////////////////////////
+		Sfmt rnd = new Sfmt(rseed);
+		double[][] X = new double[pop][2];
+		double[][] V = new double[pop][2];
+		double[] xylim=ObjFunc.GetLimit(fnum);
+		// Particle generation for their position and verocity
+		for(int i=0;i<pop;i++) {
+			for(int j=0;j<2;j++) {
+				if(j==0) {
+					X[i][j]=rnd.NextUnif()*(Math.abs(xylim[0])+Math.abs(xylim[1]))-Math.abs(xylim[0]);
+				}
+				if(j==1) {
+					X[i][j]=rnd.NextUnif()*(Math.abs(xylim[2])+Math.abs(xylim[3]))-Math.abs(xylim[2]);
+				}
+				V[i][j]=rnd.NextUnif();
+				System.out.printf("%f\t",X[i][j]);
+//				if(j==1) {
+//					System.out.printf("\n");
+//				}
+			}
+		}
+		
 		
 	}
 }
