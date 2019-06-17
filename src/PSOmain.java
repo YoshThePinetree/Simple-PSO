9a89c9a9413e5de46a377835ee77a5560498588e
YoshThePinetree <changebyone8901@gmail.com>
2019-06-18 08:03:28 +0900

    Finished until csv file export

--- src/PSOmain.java
+++ src/PSOmain.java
@@ -1,5 +1,9 @@
 // Main function
 import java.util.Random;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class PSOmain {
 	public static void main (String arg[]) {
@@ -9,8 +13,15 @@ public class PSOmain {
 		// Initial configuration //
 		///////////////////////////
 		
+		// #Benchmark number #The global minimum 
+		// 1:Rosenbrock function constrained with a cubic and a line, f(1.0,1.0)=0
+		// 2:Rosenbrock function constrained to a disk, f(1.0,1.0)=0
+		// 3:Mishra's Bird function - constrained, f(-3.1302468,-1.5821422)=-106.7645367
+		// 4:Simionescu function, f(+-0.84852813,-+0.84852813)=-0.072
+		// 5:2-D Rastrigin function, f(0,0)=0
+			
 		int fnum=2;			// the function number to solve
-		int trial=20;		// the number of trials with different random initial
+		int trial=10;		// the number of trials with different random initial
 		int ite=1000;		// the number of iterations for a trial
 		int pop=100;			// the number of particles
 		int rseed=1;
@@ -38,100 +49,135 @@ public class PSOmain {
 		///////////////////////////////////////////////
 		// Particle Generation & Initial Evaluation  //
 		///////////////////////////////////////////////
+		double[][] Frec = new double [ite][trial];
+		double[][] X1rec = new double [ite][trial];
+		double[][] X2rec = new double [ite][trial];
 		Sfmt rnd = new Sfmt(rseed);
-		double[][] X = new double[pop][2];	// the particle position matrix
-		double[][] V = new double[pop][2];	// the particle velocity matrix
-		double[] F = new double[pop];		// the objective function for the personal best
-		double[] Fpb = new double[pop];		// the current objective function
-		double[][] Pb = new double[pop][2];		// the personal best vector
-		double[] Gb = new double[2];		// the global best vector
-		double[] xylim=ObjFunc.GetLimit(fnum);
-		
-		double minfnc=1000;
-		int minind=0;
-		// Particle generation for their position and velocity
-		
-		for(int i=0;i<pop;i++) {
-			for(int j=0;j<2;j++) {
-				if(j==0) {
-					X[i][j]=rnd.NextUnif()*(Math.abs(xylim[0])+Math.abs(xylim[1]))-Math.abs(xylim[0]);
-				}
-				if(j==1) {
-					X[i][j]=rnd.NextUnif()*(Math.abs(xylim[2])+Math.abs(xylim[3]))-Math.abs(xylim[2]);
-				}
-				V[i][j]=rnd.NextUnif();
-//				System.out.printf("%f\t",X[i][j]);
-//				if(j==1) {
-//					System.out.printf("\n");
-//				}
-			}
-			F[i]=ObjFunc.EvalFunc(X[i][0], X[i][1], fnum);	// particle evaluation
-//			System.out.printf("%f\n",F[i]);
-			if(minfnc>F[i]) {
-				minfnc=F[i];
-				minind=i;
-			}
-		}
 
-		Pb=X;
-		Fpb=F;
-		for(int j=0; j<2; j++) {
-			Gb[j]=X[minind][j];
-//			System.out.printf("%f\n",Gb[j]);
-		}
-		
-
-		///////////////////////////
-		// PSO solution search   //
-		///////////////////////////
-		
-		for(int k=0; k<ite; k++) {
-			System.out.printf("Iteration %d\n",k+1);
-			// the position update
-			double[][] X1 = new double[pop][2];	// the particle position matrix
-			double[][] V1 = new double[pop][2];	// the particle velocity matrix
-			double r1, r2;
+		for(int l=0;l<trial;l++){
+			
+			double[][] X = new double[pop][2];	// the particle position matrix
+			double[][] V = new double[pop][2];	// the particle velocity matrix
+			double[] F = new double[pop];		// the objective function for the personal best
+			double[] Fpb = new double[pop];		// the current objective function
+			double[][] Pb = new double[pop][2];		// the personal best vector
+			double[] Gb = new double[2];		// the global best vector
+			double[] xylim=ObjFunc.GetLimit(fnum);
+			
+			double minfnc=1000;
+			int minind=0;
+			// Particle generation for their position and velocity
 			
 			for(int i=0;i<pop;i++) {
 				for(int j=0;j<2;j++) {
-					X1[i][j]=X[i][j] + V[i][j];	// position update
-					//System.out.printf("%f\t",X1[i][j]);
-					//if(j==1) {
-					//	System.out.printf("\n");
-					//}
-					r1=rnd.NextUnif();
-					r2=rnd.NextUnif();
-					V1[i][j]=w*V[i][j] + c1*r1*(Pb[i][j]-X[i][j]) + c2*r2*(Gb[j]-X[i][j]);	// velocity update
-					
-					if(V1[i][j]>vlim) {	// velocity limitation
-						V1[i][j]=vlim;
+					if(j==0) {
+						X[i][j]=rnd.NextUnif()*(Math.abs(xylim[0])+Math.abs(xylim[1]))-Math.abs(xylim[0]);
 					}
+					if(j==1) {
+						X[i][j]=rnd.NextUnif()*(Math.abs(xylim[2])+Math.abs(xylim[3]))-Math.abs(xylim[2]);
+					}
+					V[i][j]=rnd.NextUnif();
+	//				System.out.printf("%f\t",X[i][j]);
+	//				if(j==1) {
+	//					System.out.printf("\n");
+	//				}
 				}
-				
-				F[i]=ObjFunc.EvalFunc(X1[i][0], X1[i][1], fnum);	// particle evaluation
-//				System.out.printf("%f\n",F[i]);
-				
-				if(minfnc>F[i]) {	// the global best update
+				F[i]=ObjFunc.EvalFunc(X[i][0], X[i][1], fnum);	// particle evaluation
+	//			System.out.printf("%f\n",F[i]);
+				if(minfnc>F[i]) {
 					minfnc=F[i];
 					minind=i;
-					for(int j=0;j<2;j++) {
-						Gb[j]=X1[i][j];
-					}
-				}
-				if(Fpb[i]>F[i]) {	// the personal best update
-					Fpb[i]=F[i];
-					for(int j=0;j<2;j++) {
-						Pb[i][j]=X1[i][j];
-					}
 				}
 			}
+	
+			Pb=X;
+			Fpb=F;
+			for(int j=0; j<2; j++) {
+				Gb[j]=X[minind][j];
+	//			System.out.printf("%f\n",Gb[j]);
+			}
 			
-			X=X1;
-			V=V1;
+			///////////////////////////
+			// PSO solution search   //
+			///////////////////////////
 			
-			System.out.printf("The best fitness: \t");
-			System.out.printf("%f\n",minfnc);
+			for(int k=0; k<ite; k++) {
+				System.out.printf("Iteration %d\n",k+1);
+				// the position update
+				double[][] X1 = new double[pop][2];	// the particle position matrix
+				double[][] V1 = new double[pop][2];	// the particle velocity matrix
+				double r1, r2;
+				
+				for(int i=0;i<pop;i++) {
+					for(int j=0;j<2;j++) {
+						X1[i][j]=X[i][j] + V[i][j];	// position update
+						//System.out.printf("%f\t",X1[i][j]);
+						//if(j==1) {
+						//	System.out.printf("\n");
+						//}
+						r1=rnd.NextUnif();
+						r2=rnd.NextUnif();
+						V1[i][j]=w*V[i][j] + c1*r1*(Pb[i][j]-X[i][j]) + c2*r2*(Gb[j]-X[i][j]);	// velocity update
+						
+						if(V1[i][j]>vlim) {	// velocity limitation
+							V1[i][j]=vlim;
+						}
+					}
+					
+					F[i]=ObjFunc.EvalFunc(X1[i][0], X1[i][1], fnum);	// particle evaluation
+	//				System.out.printf("%f\n",F[i]);
+					
+					if(minfnc>F[i]) {	// the global best update
+						minfnc=F[i];
+						minind=i;
+						for(int j=0;j<2;j++) {
+							Gb[j]=X1[i][j];
+						}
+					}
+					if(Fpb[i]>F[i]) {	// the personal best update
+						Fpb[i]=F[i];
+						for(int j=0;j<2;j++) {
+							Pb[i][j]=X1[i][j];
+						}
+					}
+				}
+				
+				X=X1;
+				V=V1;
+				
+				System.out.printf("The best fitness: \t");
+				System.out.printf("%f\n",minfnc);
+				
+				Frec[k][l]=minfnc;
+				X1rec[k][l]=Gb[0];
+				X2rec[k][l]=Gb[1];
+			}
+			
 		}
 		
+		// write out the objective function value & search log
+		try {
+            // create export
+            FileWriter fw = new FileWriter("C:\\result\\PSO\\test.csv", false); // true: add, false: overwrite
+            PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+
+            // the contents of the file
+            for(int i=0;i<ite;i++) {
+				for(int j=0;j<trial;j++) {
+		            pw.print(Frec[i][j]);
+		            if(j<trial-1){
+			            pw.print(",");
+		            }else {
+			            pw.println();		            	
+		            }
+				}
+			}
+
+            // write it out
+            pw.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 	}
 }
